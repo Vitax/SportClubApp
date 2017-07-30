ea5dec58f49047fc2a98525bb31f8ce1cf619a94
vitax <caglar.oezel@googlemail.com>
2017-07-30 23:20:35 +0200

    moved Data.json into app/assets/javascripts/

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,8 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
         test 'should get home' do
                 get static_pages_home_url
                 assert_response :success
-                assert_select "title", "SportClub App"
-
+                assert_select "title", "SportClub App
         end
 
         test 'should get help' do
